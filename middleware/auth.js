4491292505341632aea7722e24b85c52bb54d8d8
MaxTyndall <maxtyndall1@gmail.com>
2026-04-14 19:47:16 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-// Middleware to check if a user is logged in
+//middleware to check if a user is logged in
 const isAuthenticated = (req, res, next) => {
     if (req.session && req.session.userId) {
-      // User is logged in, continue to the next function
+      //if user is logged in continues
       return next();
     }
-    // User is not logged in, send error
+    // send error if not logged in
     return res.status(401).json({ error: 'You must be logged in to do that.' });
   };
   
